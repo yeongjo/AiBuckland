96ac8e17932df86349dc278bf64635802d424c73
Yum <yumtr@naver.com>
2020-12-04 23:13:40 +0900

    1번과제 MemoryRecord 맞춘 적들중 체력적은 봇 타겟팅 시스템

--- VS2010/Buckland_Chapter7 to 10_Raven/Raven_TargetingSystem.cpp
+++ VS2010/Buckland_Chapter7 to 10_Raven/Raven_TargetingSystem.cpp
@@ -2,7 +2,7 @@
 #include "Raven_Bot.h"
 #include "Raven_SensoryMemory.h"
 #include "debug/DebugConsole.h"
-
+#include <algorithm>
 
 
 //-------------------------------- ctor ---------------------------------------
@@ -19,13 +19,15 @@ Raven_TargetingSystem::Raven_TargetingSystem(Raven_Bot* owner):m_pOwner(owner),
 void Raven_TargetingSystem::Update()
 {
   double ClosestDistSoFar = MaxDouble;
+  bool isCertainTarget = false;
   m_pCurrentTarget       = 0;  
   //grab a list of all the opponents the owner can sense
   std::list<Raven_Bot*> SensedBots;
-  std::vector< Raven_Bot*> HitBots;
-  bool isCertainTarget = false;
   SensedBots = m_pOwner->GetSensoryMem()->GetListOfRecentlySensedOpponents();
+  std::vector< Raven_Bot*> HitBots;
   HitBots = m_pOwner->GetSensoryMem()->GetHitbots();
+  std::sort(HitBots.begin(), HitBots.end(),
+      [](Raven_Bot* a, Raven_Bot* b) { return a->Health() > b->Health(); });
 
   std::list<Raven_Bot*>::const_iterator curBot = SensedBots.begin();
   for (curBot; curBot != SensedBots.end(); ++curBot)
@@ -34,22 +36,28 @@ void Raven_TargetingSystem::Update()
     if ((*curBot)->isAlive() && (*curBot != m_pOwner) )
     {
       double dist = Vec2DDistanceSq((*curBot)->Pos(), m_pOwner->Pos());
-      
+
+      if (m_pOwner->ID() == 401)
+      {
+          debug_con << m_pOwner->ID() << "{";
+          for (int i = 0; i < HitBots.size(); i++){ debug_con << HitBots[i]->ID() << ", "; }
+          debug_con << "}" << "";
+      }         
+
       //나를 쏘고있는가?, 나를 바라보고있는가?, 내가 쐈던 봇인가?, 가까운가, 적의 사거리가 나보다 짧은가,
       if (dist < ClosestDistSoFar)
-      {
+      {          
           for (int i = 0; i < HitBots.size(); i++)
-          {
-              if (HitBots[i] == *curBot)
-              {
-                  debug_con << m_pOwner ->ID()<<": 맞췄던놈"<< HitBots[i]->ID() <<" 부터 공격해야지" << "";
+          {             
+              if (HitBots[i] != m_pOwner && HitBots[i] == *curBot)
+              {                  
                   ClosestDistSoFar = dist;
                   m_pCurrentTarget = *curBot;
                   isCertainTarget = true;
               }
-          }
-          if (!isCertainTarget) {
-              debug_con << m_pOwner->ID() << "아무나쏴야지" << "";
+          }        
+          //타겟없음 가까운거리 적부터 타겟
+          if (!isCertainTarget) {              
               ClosestDistSoFar = dist;
               m_pCurrentTarget = *curBot;
           }          
